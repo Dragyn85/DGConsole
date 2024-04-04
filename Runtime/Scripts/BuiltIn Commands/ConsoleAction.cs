db6e772e46456f0823eb1e1a28a2d3368148669b
Dragyn85 <nygard.jonas@gmail.com>
2024-04-04 10:30:35 +0200

    Update Resize to support more resolotions, updated ConsoleAction to be more flexible

--- Runtime/Scripts/BuiltIn Commands/ConsoleAction.cs
+++ Runtime/Scripts/BuiltIn Commands/ConsoleAction.cs
@@ -6,43 +6,20 @@ namespace DragynGames.Commands
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
     public class ConsoleActionAttribute : Attribute
     {
-        private string m_command;
-        private string m_description;
-        private string[] m_parameterNames;
-
-        public string Command
-        {
-            get { return m_command; }
-        }
-
-        public string Description
-        {
-            get { return m_description; }
-        }
-
-        public string[] ParameterNames
-        {
-            get { return m_parameterNames; }
-        }
+        public string Command { get; set; }
+        public string Description { get; set; }
+        public string[] ParameterNames { get; set; }
 
         public ConsoleActionAttribute()
         {
-            m_command = null;
-            m_description = null;
+            
         }
 
         public ConsoleActionAttribute(string command, string description, params string[] parameterNames)
         {
-            m_command = command;
-            m_description = description;
-            m_parameterNames = parameterNames;
-        }
-
-        public void SetField(string Command, string Description, params string[] parameterNames)
-        {
-            m_command = Command;
-            m_description = Description;
-            m_parameterNames = parameterNames;
+            Command = command;
+            Description = description;
+            ParameterNames = parameterNames;
         }
     }
 }
--- Runtime/Scripts/Console/UI/Resize.cs
+++ Runtime/Scripts/Console/UI/Resize.cs
@@ -4,11 +4,11 @@ using UnityEngine.EventSystems;
 
 namespace DragynGames.Commands.UI
 {
-    internal class Resize : MonoBehaviour,IPointerDownHandler, IPointerUpHandler
+    internal class Resize : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         [SerializeField] private RectTransform targetWindow;
+        [SerializeField] private float responsivenessMultiplier = 1.8f; // Adjustable multiplier for responsiveness
 
-        private Vector3 windowHoldOffset;
         private bool isWindowGrabbed;
 
         private Vector2 startPosition;
@@ -19,16 +19,31 @@ namespace DragynGames.Commands.UI
             startPosition = eventData.position;
             startSize = targetWindow.sizeDelta;
             isWindowGrabbed = true;
-            StartCoroutine(MoveWindow());
+            StartCoroutine(ResizeWindow());
         }
 
-        private IEnumerator MoveWindow() {
+        private IEnumerator ResizeWindow() {
             while(isWindowGrabbed)
             {
                 Vector2 mouseDelta = startPosition - (Vector2)Input.mousePosition;
-                targetWindow.sizeDelta = new Vector2(startSize.x - mouseDelta.x,startSize.y+mouseDelta.y);
+
+                // Apply scaling based on screen resolution
+                Vector2 scaledDelta = ScaleDelta(mouseDelta);
+
+                targetWindow.sizeDelta = new Vector2(startSize.x - scaledDelta.x, startSize.y + scaledDelta.y);
                 yield return null;
             }
+        }
+
+        private Vector2 ScaleDelta(Vector2 delta) {
+            float scaleFactor = CalculateScaleFactor();
+            return delta * scaleFactor;
+        }
+
+        private float CalculateScaleFactor() {
+            float standardResolution = 1080f; // Standard resolution for scaling (1080p)
+            float maxScreenDimension = Mathf.Max(Screen.width, Screen.height);
+            return (standardResolution / maxScreenDimension) * responsivenessMultiplier;
         }
 
         public void OnPointerUp(PointerEventData eventData) {
